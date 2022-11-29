c312827fc477c5067dc0fc55fa1334fbe07de6b0
Plooto32 <ploty32@gmail.com>
2022-11-29 13:47:04 +0400

    print arguments

--- main.cpp
+++ main.cpp
@@ -7,5 +7,9 @@ int main() {
 	using namespace std;
 	SOCKET socket_tello;
 	socket_tello = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); //socket(IPv4, socket type using UDP, The UDP)
-
+	cout << "Creating a socket... " << endl;
+	cout << "AF_INET: " << AF_INET << endl;
+	cout << "SOCK_DGRAM: " << SOCK_DGRAM << endl;
+	cout << hex;
+	cout << "socket_tello: " << socket_tello << endl;
 }
