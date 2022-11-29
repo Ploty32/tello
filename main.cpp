86cae8ee7ce4809d13b58a420a84f3d611ecb723
Plooto32 <ploty32@gmail.com>
2022-11-29 13:21:31 +0400

    creating a socked

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
-int main() {}
+#include <winsock2.h>
+#include <iostream>
+#pragma comment(lib, "Ws2_32.lib")
+
+int main() {
+
+	using namespace std;
+	SOCKET socket_tello;
+	socket_tello = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); //socket(IPv4, socket type using UDP, The UDP)
+
+}
